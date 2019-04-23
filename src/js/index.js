c55a0ba09edacd94a77a9bfc522bb39cc718fe93
Patrick McMahon <pmcmahon14@gmail.com>
2019-04-22 22:16:23 -0700

    built api call with axios, async/try/catch

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,15 @@
-// Global app controller
-import num from './test';
-const x = 23;
+import axios from 'axios';
 
-console.log(`I imported ${num} from another module called test.js! Variable x is ${x}.`);
+async function getResults(query) {
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
+    const key = 'd2b0f8d8b33b9d2b3391c5e998afa817';
+    try {
+        const res = await axios(`${proxy}https://food2fork.com/api/search?key=${key}&q=${query}`);
+        const recipes = res.data.recipes;
+        console.log(recipes);
+    } catch (error) {
+        alert(error);
+    }
 
+}
+getResults('tomato pasta');
