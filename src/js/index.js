4258ebc7d066fc4d8adb7f1b707877b56b64d80d
Patrick McMahon <pmcmahon14@gmail.com>
2019-04-22 22:40:45 -0700

    built search model

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,5 @@
-import axios from 'axios';
+import Search from './models/Search';
 
-async function getResults(query) {
-    const proxy = 'https://cors-anywhere.herokuapp.com/';
-    const key = 'd2b0f8d8b33b9d2b3391c5e998afa817';
-    try {
-        const res = await axios(`${proxy}https://food2fork.com/api/search?key=${key}&q=${query}`);
-        const recipes = res.data.recipes;
-        console.log(recipes);
-    } catch (error) {
-        alert(error);
-    }
-
-}
-getResults('tomato pasta');
+const search = new Search('pizza');
+console.log(search);
+search.getResults();
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,1 +1,18 @@
-export default 'I am an exported string.';
+import axios from 'axios';
+
+export default class Seaarch {
+    constructor(query) {
+        this.query = query;
+    }
+
+    async getResults() {
+        const proxy = 'https://cors-anywhere.herokuapp.com/';
+        const key = 'd2b0f8d8b33b9d2b3391c5e998afa817';
+        try {
+            const res = await axios(`${proxy}https://food2fork.com/api/search?key=${key}&q=${this.query}`);
+            this.result = res.data.recipes;
+        } catch (error) {
+            alert(error);
+        }
+    }
+}
