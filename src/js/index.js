0f857e855d0f2af3b38b0d9265c99e7fbc4afc01
Patrick McMahon <pmcmahon14@gmail.com>
2019-04-23 18:56:42 -0700

    started building search view; can display resulting images and names and also clear results upon new search result

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,26 @@
 import Search from './models/Search';
+import * as searchView from './views/searchView';
+import {elements} from './views/base';
 
 const state = {};
 
 const controlSearch = async () => {
-    const query = 'pizza';
+    const query = searchView.getInput();
+    console.log(query);
 
     if (query) {
         state.search = new Search(query);
 
+        searchView.clearInput();
+        searchView.clearResults();
+
         await state.search.getResults();
 
-        console.log(state.search.result);
+        searchView.renderResults(state.search.result);
     }
 };
 
-document.querySelector('.search').addEventListener('submit', event => {
+elements.searchForm.addEventListener('submit', event => {
     event.preventDefault();
     controlSearch();
 });
-
-const search = new Search('pizza');
-console.log(search);
-search.getResults();
