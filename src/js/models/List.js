120b77e2dd8214f70d5489e5fb1534fd12502d53
Patrick McMahon <pmcmahon14@gmail.com>
2019-04-26 20:38:29 -0700

    built shopping model

--- src/js/models/List.js
+++ src/js/models/List.js
@@ -1,3 +1,5 @@
+import uniqid from 'uniqid';
+
 export default class List {
     constructor() {
         this.items = [];
@@ -5,9 +7,21 @@ export default class List {
 
     addItem (count, unit, ingredient) {
         const item = {
+            id: uniqid(),
             count,
             unit,
             ingredient
         }
+        this.items.push(item);
+        return item;
+    }
+
+    deleteItem(id) {
+        const index = this.items.findIndex(element => element.id === id);
+        this.items.splice(index, 1);
+    }
+
+    updateCount(id, newCount) {
+        this.items.find(element => element.id === id).count = newCount;
     }
 }
